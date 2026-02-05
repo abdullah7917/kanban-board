d06fe6bc4fff8de9f05b7d869c7cdfc06b43e92c
Abdullah ali khan <abdullah@MacBook-Air.local>
2026-02-04 20:45:19 -0800

    fix: nhost config

--- src/lib/nhost.ts
+++ src/lib/nhost.ts
@@ -3,5 +3,4 @@ import { NhostClient } from "@nhost/nhost-js";
 export const nhost = new NhostClient({
   subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN!,
   region: process.env.NEXT_PUBLIC_NHOST_REGION!,
-  graphqlUrl: process.env.NEXT_PUBLIC_NHOST_GRAPHQL_URL!,
 });
